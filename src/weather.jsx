01c25116aa26c17975e9d3491b4889cb6477dbfc
Srinivas883 <srinivassampath883@gmail.com>
2025-01-04 10:21:49 +0530

    updated with images

--- src/weather.jsx
+++ src/weather.jsx
@@ -1,13 +1,53 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios"
 
 function Weatherapp() {
 
-    const [city, setcity] = useState("")
-
+    const [city, setcity] = useState("New Delhi")
     const [weather, setweather] = useState("")
     const [temp, settemp] = useState("")
-    const [desc,setdesc]=useState("")
+    const [desc, setdesc] = useState("")
+    const [weatherimg, setweatherimg] = useState()
+
+    const weatherimgcode = {
+        Thunderstorm: "11d",
+        Drizzle: "09d",
+        Rain: "10d",
+        Snow: "13d",
+        Mist: "50d",
+        Smoke: "50d",
+        Haze: "50d",
+        Dust: "50d",
+        Fog: "50d",
+        Sand: "50d",
+        Ash: "50d",
+        Squall: "50d",
+        Tornado: "50d",
+        Clear: "01d",
+        "few clouds": "02d",
+        "scattered clouds": "03d",
+        "broken clouds": "04d",
+        "overcast clouds": "04d"
+    }
+
+useEffect(()=>{
+
+    var weatherdata = axios(`https://api.openweathermap.org/data/2.5/weather?q=New Delhi&appid=aa3b687b8bbb078d3975636723b7b2ad`)
+
+        weatherdata.then(function (success) {
+            setweather(success.data.weather[0].main)
+            settemp((success.data.main.temp - 273.15).toFixed(0) + "°C")
+            setdesc(success.data.weather[0].description)
+            console.log(success.data.weather[0].main)
+            if (success.data.weather[0].main == "Clouds") {
+                setweatherimg(`https://openweathermap.org/img/wn/${weatherimgcode[success.data.weather[0].description]}@2x.png`)
+            } else {
+                setweatherimg(`https://openweathermap.org/img/wn/${weatherimgcode[success.data.weather[0].main]}@2x.png`)
+            }
+
+
+        }).catch(() => { alert("Incorrect name of the city. Please input correct value") })
+},[])
 
     function handlecity(evt) {
         setcity(evt.target.value)
@@ -19,22 +59,32 @@ function Weatherapp() {
 
         weatherdata.then(function (success) {
             setweather(success.data.weather[0].main)
-            settemp((success.data.main.temp-273.15).toFixed(0)+"°C")
+            settemp((success.data.main.temp - 273.15).toFixed(0) + "°C")
             setdesc(success.data.weather[0].description)
-        }).catch(()=>{alert("Incorrect name of the city. Please input correct value")})
+            console.log(success.data.weather[0].main)
+            if (success.data.weather[0].main == "Clouds") {
+                setweatherimg(`https://openweathermap.org/img/wn/${weatherimgcode[success.data.weather[0].description]}@2x.png`)
+            } else {
+                setweatherimg(`https://openweathermap.org/img/wn/${weatherimgcode[success.data.weather[0].main]}@2x.png`)
+            }
+
+
+        }).catch(() => { alert("Incorrect name of the city. Please input correct value") })
+
 
     }
 
     return (
         <>
-            <div className="bg-black p-20">
+            <div className="blackbackground">
 
-                <div className="bg-green-400 p-10 rounded-md">
+                <div className="greenbackground">
 
-                    <h1 className="text-2xl font-medium">Weather Report</h1>
+                    <h1 className="heading">Weather Report</h1>
                     <p>Get the weather report about your city!</p>
-                    <input value={city} onChange={handlecity} type="text" className="mt-2 p-1 border border-black rounded" placeholder="Type city name.." />
-                    <button onClick={getWeather} className="bg-black text-white p-2 rounded-md mt-2 block">Get Report</button>
+                    <img className="weatherimg" src={weatherimg} alt="" />
+                    <input value={city} onChange={handlecity} type="text" className="inputbox" placeholder="Type city name.." />
+                    <button onClick={getWeather} className="getreportbtn">Get Report</button>
 
                     <div className="mt-2">
                         <p><b>Weather:</b> {weather}</p>
